648372f38bddf1ab55ed3bcd7a10bc70d6d85106
Germey <cqc@cuiqingcai.com>
2018-03-20 02:11:35 +0800

    rm readme

--- data.py
+++ data.py
@@ -3,16 +3,25 @@ from train import FLAGS
 
 
 def main():
-    train_set = BiTextIterator(source=FLAGS.source_train_data,
-                               target=FLAGS.target_train_data,
+    train_set = BiTextIterator(source='dataset/q1q2/request_1000.txt',
+                               target='dataset/q1q2/response_1000.txt',
                                source_dict=FLAGS.source_vocabulary,
                                target_dict=FLAGS.target_vocabulary,
-                               batch_size=FLAGS.batch_size,
+                               batch_size=128,
                                max_length=FLAGS.max_seq_length,
                                n_words_source=FLAGS.num_encoder_symbols,
                                n_words_target=FLAGS.num_decoder_symbols,
                                sort_by_length=FLAGS.sort_by_length
                                )
+    train_set.reset()
+    
+    for source, target in train_set.next():
+        print('Length', len(source), len(target))
+    
+    train_set.reset()
+    
+    print('Reset')
+    
     for source, target in train_set.next():
         print('Length', len(source), len(target))
 
