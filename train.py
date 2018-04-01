5b308f90a6eb84d451be3232faabc055838c0424
Germey <cqc@cuiqingcai.com>
2018-04-02 01:21:55 +0800

    finish train

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ tf.app.flags.DEFINE_integer('depth', 2, 'Number of layers in each encoder and de
 tf.app.flags.DEFINE_integer('embedding_size', 200, 'Embedding dimensions of encoder and decoder inputs')
 tf.app.flags.DEFINE_integer('num_encoder_symbols', 6564, 'Source vocabulary size')
 tf.app.flags.DEFINE_integer('num_decoder_symbols', 6564, 'Target vocabulary size')
-
+tf.app.flags.DEFINE_string('split_sign', ' ', 'Separator of dataset')
 tf.app.flags.DEFINE_boolean('use_residual', True, 'Use residual connection between layers')
 tf.app.flags.DEFINE_boolean('attn_input_feeding', False, 'Use input feeding method in attentional decoder')
 tf.app.flags.DEFINE_boolean('use_dropout', True, 'Use dropout in each rnn cell')
@@ -47,7 +47,7 @@ tf.app.flags.DEFINE_integer('batch_size', 64, 'Batch size')
 tf.app.flags.DEFINE_integer('max_epochs', 10000, 'Maximum # of training epochs')
 tf.app.flags.DEFINE_integer('max_load_batches', 20, 'Maximum # of batches to load at one time')
 tf.app.flags.DEFINE_integer('max_seq_length', 20, 'Maximum sequence length')
-tf.app.flags.DEFINE_integer('display_freq', 300, 'Display training status every this iteration')
+tf.app.flags.DEFINE_integer('display_freq', 100, 'Display training status every this iteration')
 tf.app.flags.DEFINE_integer('save_freq', 30000, 'Save model checkpoint every this iteration')
 tf.app.flags.DEFINE_integer('valid_freq', 1000, 'Evaluate model every this iteration: valid_data needed')
 tf.app.flags.DEFINE_string('optimizer', 'adam', 'Optimizer for training: (adadelta, adam, rmsprop)')
@@ -96,7 +96,8 @@ def train():
                                max_length=FLAGS.max_seq_length,
                                n_words_source=FLAGS.num_encoder_symbols,
                                n_words_target=FLAGS.num_decoder_symbols,
-                               sort_by_length=FLAGS.sort_by_length
+                               sort_by_length=FLAGS.sort_by_length,
+                               split_sign=FLAGS.split_sign
                                )
     
     if FLAGS.source_valid_data and FLAGS.target_valid_data:
@@ -109,7 +110,9 @@ def train():
                                    max_length=FLAGS.max_seq_length,
                                    n_words_source=FLAGS.num_encoder_symbols,
                                    n_words_target=FLAGS.num_decoder_symbols,
-                                   sort_by_length=FLAGS.sort_by_length)
+                                   sort_by_length=FLAGS.sort_by_length,
+                                   split_sign=FLAGS.split_sign
+                                   )
     else:
         valid_set = None
     
@@ -147,6 +150,9 @@ def train():
                     source, source_len, target, target_len = prepare_pair_batch(source_seq, target_seq,
                                                                                 FLAGS.max_seq_length,
                                                                                 FLAGS.max_seq_length)
+                    
+                    # print('Get Data', source.shape, target.shape, source_len.shape, target_len.shape)
+                    # print('Data', list(source[0]), list(target[0]))
                     
                     processed_number += len(source_seq)
                     
